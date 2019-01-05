5007d4034591a9c5634ae21d6705c560625cde3e
wxwu <scottwu7266@gmail.com>
2019-01-05 18:06:05 +0800

    add Junit Test test functionality of GET request

--- src/test/java/com/ficus/face/product/spring/tutorial/simplewebservice/controller/StudentControllerTest.java
+++ src/test/java/com/ficus/face/product/spring/tutorial/simplewebservice/controller/StudentControllerTest.java
@@ -1,30 +1,37 @@
 package com.ficus.face.product.spring.tutorial.simplewebservice.controller;
 
-import com.ficus.face.product.spring.tutorial.simplewebservice.models.Student;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(StudentController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class StudentControllerTest {
 
-//    @Autowired
-//    private MockMvc mvc;
-//
-//    @MockBean
-//    private StudentController studentController;
-//
-//    @Test
-//    public void
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void StudentControllerTest() throws Exception {
+        this.mvc.perform(get("/students/")).andDo(print()).andExpect(status().isOk())
+        .andExpect(content().string(containsString("Alice")));
+        this.mvc.perform(get("/students/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Bob")));
+    }
 }
